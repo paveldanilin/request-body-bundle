2667d1fa06b3292a7103a8df6fecc73e8ccdf442
Pavel <pavel.k.danilin@gmail.com>
2021-11-28 22:13:23 +1000

    Add cache

---
+++ src/Cache/RequestBodyWarmer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Pada\RequestBodyBundle\Cache;
+
+use Pada\Reflection\Scanner\ScannerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+
+class RequestBodyWarmer implements CacheWarmerInterface
+{
+    private ScannerInterface $scanner;
+    private ParameterBagInterface $parameterBag;
+
+    public function __construct(ScannerInterface $scanner, ParameterBagInterface $parameterBag)
+    {
+        $this->scanner = $scanner;
+        $this->parameterBag = $parameterBag;
+    }
+
+    public function isOptional(): bool
+    {
+        return true;
+    }
+
+    public function warmUp(string $cacheDir): array
+    {
+        print '>>' . $cacheDir . "]]\n";
+
+        $projectDir = $this->parameterBag->get('kernel.project_dir');
+        $this->scanner->in($projectDir . DIRECTORY_SEPARATOR . 'src');
+
+        return [];
+    }
+}
