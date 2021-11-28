f1c6fff31706930a9a4c6767c3d1dca8cd1406f0
Pavel <pavel.k.danilin@gmail.com>
2021-11-28 22:38:15 +1000

    improve warmer

--- src/Cache/RequestBodyWarmer.php
+++ src/Cache/RequestBodyWarmer.php
@@ -2,7 +2,10 @@
 
 namespace Pada\RequestBodyBundle\Cache;
 
+use Pada\Reflection\Scanner\ClassInfo;
 use Pada\Reflection\Scanner\ScannerInterface;
+use Pada\RequestBodyBundle\Controller\Annotation\RequestBody;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
 
@@ -10,11 +13,13 @@ class RequestBodyWarmer implements CacheWarmerInterface
 {
     private ScannerInterface $scanner;
     private ParameterBagInterface $parameterBag;
+    private CacheItemPoolInterface $cacheSystem;
 
-    public function __construct(ScannerInterface $scanner, ParameterBagInterface $parameterBag)
+    public function __construct(ScannerInterface $scanner, ParameterBagInterface $parameterBag, CacheItemPoolInterface $cacheSystem)
     {
         $this->scanner = $scanner;
         $this->parameterBag = $parameterBag;
+        $this->cacheSystem = $cacheSystem;
     }
 
     public function isOptional()
@@ -24,10 +29,19 @@ class RequestBodyWarmer implements CacheWarmerInterface
 
     public function warmUp($cacheDir)
     {
-        print '>>' . $cacheDir . "]]\n";
-
         $projectDir = $this->parameterBag->get('kernel.project_dir');
-        $this->scanner->in($projectDir . DIRECTORY_SEPARATOR . 'src');
+
+        /** @var ClassInfo $classInfo */
+        foreach ($this->scanner->in($projectDir . DIRECTORY_SEPARATOR . 'src') as $classInfo) {
+            foreach ($classInfo->getMethodNames() as $methodName) {
+                foreach ($classInfo->getMethodAnnotations($methodName) as $annotation) {
+                    if ($annotation instanceof RequestBody) {
+                        $key = $classInfo->getReflection()->getName() . '_' . $methodName;
+                        print $key . "\n";
+                    }
+                }
+            }
+        }
 
         return [];
     }
